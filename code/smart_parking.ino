416c5a130adcdd3f2a25a5b70041f6ca33727d21
KAVYA  BASAVARAJ RAMASALI <ramasalikavya@gmail.com>
2026-05-11 07:48:22 +0530

    Update smart_parking.ino

--- code/smart_parking.ino
+++ code/smart_parking.ino
@@ -1,4 +1,3 @@
-```c
 #define TRIG_PIN 9
 #define ECHO_PIN 10
 
@@ -8,45 +7,53 @@ float distance;
 const int thresholdDistance = 15;
 
 void setup() {
-
     Serial.begin(9600);
 
     pinMode(TRIG_PIN, OUTPUT);
     pinMode(ECHO_PIN, INPUT);
 
-    Serial.println("SMART CAR PARKING SYSTEM");
+    digitalWrite(TRIG_PIN, LOW);
+
+    Serial.println("SMART CAR PARKING SYSTEM STARTED");
 }
 
 void loop() {
 
-    distance = measureDistance();
+    distance = getDistance();
 
     Serial.print("Distance: ");
     Serial.print(distance);
     Serial.println(" cm");
 
-    if(distance < thresholdDistance) {
+    if (distance == -1) {
+        Serial.println("STATUS: SENSOR ERROR");
+    }
+    else if (distance < thresholdDistance) {
         Serial.println("STATUS: SLOT OCCUPIED");
     }
     else {
         Serial.println("STATUS: SLOT AVAILABLE");
     }
 
-    delay(1000);
+    Serial.println("------------------------");
+
+    delay(500);
 }
 
-float measureDistance() {
+float getDistance() {
 
     digitalWrite(TRIG_PIN, LOW);
     delayMicroseconds(2);
 
     digitalWrite(TRIG_PIN, HIGH);
     delayMicroseconds(10);
-
     digitalWrite(TRIG_PIN, LOW);
 
-    duration = pulseIn(ECHO_PIN, HIGH);
+    duration = pulseIn(ECHO_PIN, HIGH, 30000);
+
+    if (duration == 0) {
+        return -1; // no echo received
+    }
 
     return duration * 0.034 / 2;
 }
-```
